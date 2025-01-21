104a19fc2858fdcc0646d0469706917b07385fa4
seomi <seomi.lee@gmail.com>
2025-01-22 00:29:33 +0900

    :lipstick: 기본 스타일 지정 - color - font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,38 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slate: {
+          900: "#0F172A",
+          800: "#1E293B",
+          500: "#64748B",
+          400: "#94A3B8",
+          300: "#CBD5E1",
+          200: "#E2E8F0",
+          100: "#F1F5F9",
+        },
+        violet: {
+          600: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        rose: {
+          500: "#F43F5E",
+        },
+        lime: {
+          300: "#BEF264",
+        },
+        amber: {
+          800: "#92400E",
+        },
+      },
+      fontFamily: {
+        nanum: ["NanumSquare", "sans-serif"],
+      },
+      fontSize: {
+        "bold-20": ["20px", { lineHeight: "22px", fontWeight: "700" }],
+        "bold-18": ["18px", { lineHeight: "20px", fontWeight: "700" }],
+        "extra-bold-16": ["16px", { lineHeight: "18px", fontWeight: "800" }],
+        "bold-16": ["16px", { lineHeight: "18px", fontWeight: "700" }],
+        "regular-16": ["16px", { lineHeight: "18px", fontWeight: "400" }],
       },
     },
   },
