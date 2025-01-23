c19fb9baf5b4f1f2b1380472a0452da5b5cd7909
seomi <seomi.lee@gmail.com>
2025-01-23 18:42:30 +0900

    :hammer: 할일 추가하기 파트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import TextButton from "./component/Button/TextButton";
+import TodoInput from "./component/TodoInput";
+import AddIcon from "../assets/icon/plus-white.svg";
+import { ButtonType } from "./utils/type";
+
 export default function Home() {
-  return <div>HOME</div>;
+  return (
+    <>
+      <div className="flex gap-[8px]">
+        <TodoInput />
+        <TextButton
+          icon={<AddIcon />}
+          type={ButtonType.PRIMARY}
+          text={"추가하기"}
+          style={"flex-none text-white"}
+        />
+      </div>
+    </>
+  );
 }
