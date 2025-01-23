e4118d04e2c6f7a138e7e591f3fab5270839fa7a
seomi <seomi.lee@gmail.com>
2025-01-23 19:32:41 +0900

    :lipstick: todo list 파트에 미디어 쿼리 적용

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -33,33 +33,35 @@ export default function Home() {
           style={`flex-none ${tempCheckListItems.length === 0 && "text-white"}`}
         />
       </div>
-      <div className="mt-[24px]">
-        <Image src="/image/todo@2x.png" alt="todo" width={101} height={36} />
-        <ul className="flex flex-col gap-[16px] mt-[16px]">
-          {tempCheckListItems
-            .filter((item) => item.isDone === false)
-            .map((item) => (
-              <CheckListItem
-                key={item.id}
-                title={item.title}
-                isDone={item.isDone}
-              />
-            ))}
-        </ul>
-      </div>
-      <div className="mt-[48px]">
-        <Image src="/image/done@2x.png" alt="todo" width={101} height={36} />
-        <ul className="flex flex-col gap-[16px] mt-[16px]">
-          {tempCheckListItems
-            .filter((item) => item.isDone === true)
-            .map((item) => (
-              <CheckListItem
-                key={item.id}
-                title={item.title}
-                isDone={item.isDone}
-              />
-            ))}
-        </ul>
+      <div className={`flex flex-col xl:flex-row xl:gap-[24px] xl:mt-[40px]`}>
+        <div className="mt-[24px] xl:mt-0 xl:w-[588px]">
+          <Image src="/image/todo@2x.png" alt="todo" width={101} height={36} />
+          <ul className="flex flex-col gap-[16px] mt-[16px]">
+            {tempCheckListItems
+              .filter((item) => item.isDone === false)
+              .map((item) => (
+                <CheckListItem
+                  key={item.id}
+                  title={item.title}
+                  isDone={item.isDone}
+                />
+              ))}
+          </ul>
+        </div>
+        <div className="mt-[48px] xl:w-[588px] xl:mt-0">
+          <Image src="/image/done@2x.png" alt="todo" width={101} height={36} />
+          <ul className="flex flex-col gap-[16px] mt-[16px]">
+            {tempCheckListItems
+              .filter((item) => item.isDone === true)
+              .map((item) => (
+                <CheckListItem
+                  key={item.id}
+                  title={item.title}
+                  isDone={item.isDone}
+                />
+              ))}
+          </ul>
+        </div>{" "}
       </div>
     </>
   );
