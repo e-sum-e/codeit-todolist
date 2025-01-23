62562f748e52731d0e822e73cdd1dbfffbf9e237
seomi <seomi.lee@gmail.com>
2025-01-23 19:54:11 +0900

    :hammer: todo empty 스타일 추가

--- src/app/(home)/Todos.tsx
+++ src/app/(home)/Todos.tsx
@@ -6,19 +6,45 @@ interface Props {
   items: CheckListItemType[];
 }
 
-export default function Todos({ items: todoItems }: Props) {
+export default function Todos({ items }: Props) {
   return (
     <div className="mt-[24px] xl:mt-0 xl:w-[588px]">
       <Image src="/image/todo@2x.png" alt="todo" width={101} height={36} />
-      <ul className="flex flex-col gap-[16px] mt-[16px]">
-        {todoItems.map((item) => (
-          <CheckListItem
-            key={item.id}
-            title={item.title}
-            isDone={item.isDone}
-          />
-        ))}
-      </ul>
+      {items.length < 1 ? (
+        <div className="flex flex-col items-center w-[fit-content] m-[auto]">
+          <div className="md:hidden">
+            <Image
+              src="/image/empty-todo-small@2x.png"
+              alt="empty-todo"
+              width={120}
+              height={120}
+            />
+          </div>
+          <div className="sm:hidden md:block">
+            <Image
+              src="/image/empty-todo-large@2x.png"
+              alt="empty-todo"
+              width={240}
+              height={240}
+            />
+          </div>
+          <div
+            className={`font-bold-16 text-slate-400 text-center whitespace-pre-line`}
+          >
+            할 일이 없어요.{"\n"}TODO를 새롭게 추가해주세요!
+          </div>
+        </div>
+      ) : (
+        <ul className="flex flex-col gap-[16px] mt-[16px]">
+          {items.map((item) => (
+            <CheckListItem
+              key={item.id}
+              title={item.title}
+              isDone={item.isDone}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,15 +4,14 @@ import Dones from "./Dones";
 
 const tempCheckListItems = [
   { id: "1", title: "비타민 챙겨먹기", isDone: true },
-  { id: "2", title: "맥주 마시기", isDone: false },
-  { id: "3", title: "운동하기", isDone: false },
+  { id: "2", title: "맥주 마시기", isDone: true },
+  { id: "3", title: "운동하기", isDone: true },
 ];
 
 export default function Home() {
   return (
     <>
       <AddTodo checkListItems={tempCheckListItems} />
-
       <div className={`flex flex-col xl:flex-row xl:gap-[24px] xl:mt-[40px]`}>
         <Todos
           items={tempCheckListItems.filter((item) => item.isDone === false)}
