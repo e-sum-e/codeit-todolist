c99ee26283725ab1b2dd8c1520990956485dd5b5
seomi <seomi.lee@gmail.com>
2025-01-24 18:09:07 +0900

    :heavy_plus_sign: 외부 이미지 허용을 위해 사용할 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"], // 외부 이미지 도메인을 허용
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
